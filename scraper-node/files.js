f599d5493d318a8368e1e4c7cfa9c66817f8bec8
jpaddeo <jpaddeo@gmail.com>
2022-04-21 12:37:50 -0300

    Fixing files order to fetch. Adding every 4 hours to wkf

--- scraper-node/files.js
+++ scraper-node/files.js
@@ -3,12 +3,14 @@ import path from 'path';
 
 export const getMostRecentFile = (dir) => {
   const files = orderReccentFiles(dir);
-  return files.length ? files[0].file : undefined;
+  return files.length ? files[0] : undefined;
 };
 const orderReccentFiles = (dir) => {
   return fs
     .readdirSync(dir)
-    .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
-    .map((file) => ({ file, ctime: fs.lstatSync(path.join(dir, file)).ctime }))
-    .sort((a, b) => b.ctime.getTime() - a.ctime.getTime());
+    .filter(
+      (file) =>
+        fs.lstatSync(path.join(dir, file)).isFile() && file !== 'index.json'
+    )
+    .sort((a, b) => b.localeCompare(a));
 };
