0375edb0633c5647cdf1e68eb473b2556fc2a366
jpaddeo <jpaddeo@gmail.com>
2022-05-06 17:52:46 -0300

    Adding algolia autoindexer.

--- frontend/components/LatestComics.jsx
+++ frontend/components/LatestComics.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import Comic from './Comic';
@@ -12,6 +13,32 @@ const LatestComics = ({ latestComics }) => {
       <h2 className='text-3xl font-bold text-center mb-10'>
         {_T('LATEST_COMICS')}
       </h2>
+      {/*
+      <section className='overflow-hidden text-gray-700'>
+        <div className='container px-5 py-2 mx-auto lg:pt-12 lg:px-32'>
+          <div className='flex flex-wrap -m-1 md:-m-2 gap-1'>
+            {latestComics.map((comic) => (
+              <Link href={`/comic/${comic.id}`}>
+                <a href={comic.link}>
+                  <div key={comic.id} className='flex flex-wrap w-1/3 bg-white'>
+                    <div className='w-full p-2 md:p-4'>
+                      <Image
+                        layout='intrinsic'
+                        objectFit='contain'
+                        width={comic.width}
+                        height={comic.height}
+                        src={comic.img}
+                        alt={comic.alt}
+                      />
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+      */}
       <section className='grid grid-cols-1 md:grid-cols-2 divide-x-4 divide-y-4 divide-gray-300'>
         {latestComics.map((comic) => (
           <article key={comic.id}>
---
+++ updater-algolia-node/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+import algoliasearch from 'algoliasearch';
+import fs from 'fs-extra';
+
+import StreamArray from 'stream-json/streamers/StreamArray';
+
+const algoliaClient = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_APP_KEY
+);
+
+const comicsIndex = algoliaClient.initIndex('comics');
+
+const COMICS_FRONTEND_FOLDER_PATH = '../frontend/comics';
+
+const stream = fs
+  .createReadStream(`${COMICS_FRONTEND_FOLDER_PATH}/index.json`)
+  .pipe(StreamArray.withParser());
+
+stream
+  .on('data', ({ value }) => {
+    chunks.push(value);
+    if (chunks.length === 10000) {
+      stream.pause();
+      comicsIndex
+        .saveObjects(chunks, { autoGenerateObjectIDIfNotExist: true })
+        .then(() => {
+          chunks = [];
+          stream.resume();
+        })
+        .catch(console.error);
+    }
+  })
+  .on('end', () => {
+    if (chunks.length) {
+      comicsIndex
+        .saveObjects(chunks, {
+          autoGenerateObjectIDIfNotExist: true,
+        })
+        .catch(console.error);
+    }
+  })
+  .on('error', (err) => console.error(err));
